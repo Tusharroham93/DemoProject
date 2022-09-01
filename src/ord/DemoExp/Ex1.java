6ae904d41bb887ce5d3af19c20309a8b7b67d928
Tusharroham93 <rohamtushar143@gmail.com>
2022-09-01 15:28:40 +0530

    first commit....

---
+++ src/ord/DemoExp/Ex1.java
@@ -0,0 +1,16 @@
+package ord.DemoExp;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+
+		for(int i=0;i<=4;i++) {
+			for(int j=65;j<=68;j++) {
+				System.out.print("  ");
+				System.out.print((char)j);
+			}
+			System.out.println();
+		}
+	}
+
+}
